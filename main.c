d4d370f40bba146c73a94df1419e247411b9da1e
LowBassMan <github.com@mattalexander.com>
2014-11-30 12:15:07 -0800

    tab formatting fixes

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include <conio.h>		//getch && kbhit
+#include <conio.h>      //getch && kbhit
 #include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>	//for setting window sizes and moving to console locations
+#include <Windows.h>    //for setting window sizes and moving to console locations
 #include "datfile.h"
 #include "applyRules.h"
-#include "dirent.h"		//for listing directory contents
+#include "dirent.h"     //for listing directory contents
 #include "globals.h"
 
 void loadFile(char* datfile);
@@ -14,56 +14,56 @@ int displayRunningMenu(void);
 int printGrid(const char gridCurr[][COLS], const char gridNext[][COLS]);
 
 int main(void) {
-	char datfile[FILENAME_MAX];			//starting world filename
-	char gridA[ROWS][COLS];				//create two arrays for the current and next state
-	char gridB[ROWS][COLS];
-	char (*gridPtrCurr)[COLS] = NULL;	//pointer to the currently displayed array
-	char (*gridPtrNext)[COLS] = NULL;	//pointer to the next array to be used
-	char (*tmpPtr)[COLS] = NULL;		//temporary pointer used for pointer swap
-	char ch = ' ';						//getch holder when running
-	int quit = 0;						//"bool" to show start screen
-	int count = 0;						//generation counter
-	int i = 0;
-	int j = 0;
+    char datfile[FILENAME_MAX];         //starting world filename
+    char gridA[ROWS][COLS];             //create two arrays for the current and next state
+    char gridB[ROWS][COLS];
+    char (*gridPtrCurr)[COLS] = NULL;   //pointer to the currently displayed array
+    char (*gridPtrNext)[COLS] = NULL;   //pointer to the next array to be used
+    char (*tmpPtr)[COLS] = NULL;        //temporary pointer used for pointer swap
+    char ch = ' ';                      //getch holder when running
+    int quit = 0;                       //"bool" to show start screen
+    int count = 0;                      //generation counter
+    int i = 0;
+    int j = 0;
 
-	////Windows.h call:  set the window size when it loads so we can see everything
-	SMALL_RECT windowSize = {0 , 0 , COLS , ROWS + 3};
-	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &windowSize);
+    ////Windows.h call:  set the window size when it loads so we can see everything
+    SMALL_RECT windowSize = {0 , 0 , COLS , ROWS + 3};
+    SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &windowSize);
 
-	for (i = 0; i < ROWS; i++) {
-		for(j = 0; j < COLS; j++) {
-			gridA[i][j] = ' ';
-			gridB[i][j] = ' ';
-		}
-	}
-	strcpy(datfile, ".\\worlds\\welcome.dat");
-    
-	readDatFile(datfile, gridA, ROWS);
-	gridPtrCurr = gridA;
-	gridPtrNext = gridB;
-	printGrid(gridPtrCurr, gridPtrNext);
-	puts("");
-    
-	displayMenu();
-	gotoxy(0,0);
-    
-	_sleep(3500);  //this currently blocks UI, Fix this
+    for (i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            gridA[i][j] = ' ';
+            gridB[i][j] = ' ';
+        }
+    }
+    strcpy(datfile, ".\\worlds\\welcome.dat");
 
-	//Should we put the game logic in its own function outside of MAIN?
-	//that way we can call it whenever/however we want  (such as restarting, etc)
-	while (!quit) {
-		if (count == 170) { //When welcome.dat becomes stagnent, start it over
+    readDatFile(datfile, gridA, ROWS);
+    gridPtrCurr = gridA;
+    gridPtrNext = gridB;
+    printGrid(gridPtrCurr, gridPtrNext);
+    puts("");
+
+    displayMenu();
+    gotoxy(0,0);
+
+    _sleep(3500);  //this currently blocks UI, Fix this
+
+    //Should we put the game logic in its own function outside of MAIN?
+    //that way we can call it whenever/however we want  (such as restarting, etc)
+    while (!quit) {
+        if (count == 170) { //When welcome.dat becomes stagnent, start it over
             count = 0;
             readDatFile(datfile, gridA, ROWS);
             printGrid(gridPtrCurr, gridPtrNext);
         }
         printGrid(gridPtrCurr, gridPtrNext);
-		applyRules(gridPtrCurr, gridPtrNext, ROWS);
-		
-		// swap array pointers
-		tmpPtr = gridPtrCurr;
-		gridPtrCurr = gridPtrNext;
-		gridPtrNext = tmpPtr;
+        applyRules(gridPtrCurr, gridPtrNext, ROWS);
+
+        // swap array pointers
+        tmpPtr = gridPtrCurr;
+        gridPtrCurr = gridPtrNext;
+        gridPtrNext = tmpPtr;
 
         gotoxy(COLS - 9, ROWS + 1);
         printf("Gen: %4d", count++);
@@ -83,12 +83,12 @@ int main(void) {
                 break;
 
             case 'l':
-				loadFile(datfile);
-				quit = 1;
-				break;
+                loadFile(datfile);
+                quit = 1;
+                break;
 
             case 'r':
-            	createRandDatFile("worlds/datfile.dat");
+                createRandDatFile("worlds/datfile.dat");
                 strcpy(datfile, ".\\worlds\\datfile.dat");
                 quit = 1;
                 break;
@@ -96,48 +96,47 @@ int main(void) {
             default:
                 break;
             }
-        } 
+        }
     }
 
-	readDatFile(datfile, gridA, ROWS);
-	gridPtrCurr = gridA;
-	gridPtrNext = gridB;
-	count = 0;
-	displayRunningMenu();
+    readDatFile(datfile, gridA, ROWS);
+    gridPtrCurr = gridA;
+    gridPtrNext = gridB;
+    count = 0;
+    displayRunningMenu();
     do {
         gotoxy(COLS - 9, ROWS + 1);
         printf("Gen: %4d", count++);
 
-		applyRules(gridPtrCurr, gridPtrNext, ROWS);
-		printGrid(gridPtrCurr, gridPtrNext);
+        applyRules(gridPtrCurr, gridPtrNext, ROWS);
+        printGrid(gridPtrCurr, gridPtrNext);
 
-		// swap pointers
-		tmpPtr = gridPtrCurr;
-		gridPtrCurr = gridPtrNext;
-		gridPtrNext = tmpPtr;
+        // swap pointers
+        tmpPtr = gridPtrCurr;
+        gridPtrCurr = gridPtrNext;
+        gridPtrNext = tmpPtr;
 
-		ch = getch();
-	} while(ch != KEY_ESC);
+        ch = getch();
+    } while(ch != KEY_ESC);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 void loadFile(char* datfile) {
-	char filename[FILENAME_MAX];
+    char filename[FILENAME_MAX];
 
-	system("cls");
-	printf("Please choose a file\n");
-	displayFiles();
+    system("cls");
+    printf("Please choose a file\n");
+    displayFiles();
 
-	fgets(filename, FILENAME_MAX, stdin);
-	/* remove the newline from the filename and terminate string */
-	if (filename[strlen(filename) - 1] == '\n')
-		filename[strlen(filename) - 1] = '\0';
-	else
-		while (getchar() != '\n')
-			;
-	strcpy(datfile, ".\\worlds\\");
-	strcat(datfile, filename);
+    fgets(filename, FILENAME_MAX, stdin);
+    if (filename[strlen(filename) - 1] == '\n') //remove newline
+        filename[strlen(filename) - 1] = '\0';  //terminate string
+    else
+        while (getchar() != '\n')
+            ;
+    strcpy(datfile, ".\\worlds\\");
+    strcat(datfile, filename);
 }
 
 int displayFiles(void) {
@@ -183,16 +182,16 @@ int displayRunningMenu(void) {
 }
 
 int printGrid(const char gridCurr[][COLS], const char gridNext[][COLS]) {
-	int i = 0, j = 0;
-	char ch = ' ';
+    int i = 0, j = 0;
+    char ch = ' ';
 
-	for (i = 0; i < ROWS; i++) {
-		for (j = 0; j < COLS; j++) {
-			gotoxy(j, i);	//(x) columns, (y)row
-			printf("%c", gridCurr[i][j]);
-		}
-	}
-	gotoxy(COLS, ROWS);
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            gotoxy(j, i);    //(x) columns, (y)row
+            printf("%c", gridCurr[i][j]);
+        }
+    }
+    gotoxy(COLS, ROWS);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
