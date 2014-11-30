7e50cd1454164b4a62e7c91018bafbbbe0a456c5
LowBassMan <github.com@mattalexander.com>
2014-11-29 18:41:47 -0800

    Cleanup

--- Game-of-Life/applyRules.h
+++
@@ -1,5 +0,0 @@
-#include "globals.h"
-
-void applyRules(char gridCurr[][COLS], char gridNext[][COLS], int rows);
-//void printGrid(char gridCurr[][COLS], int rows);
-void setEqualTo(char gridCurr[][COLS], char gridNext[][COLS], int rows);
