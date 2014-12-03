ae2f6cb499cb2c670a8e4dc946dd02fe8ea838fc
Littke <ian.littke@gmail.com>
2014-12-02 22:39:12 -0800

    fix build break

--- applyRules.h
+++ applyRules.h
@@ -1,4 +1,4 @@
 #include "globals.h"
 
-void applyRule(const char gridCurr[][COLS],const char gridNext[][COLS],const int rule[2][9]);
+void applyRule(char gridCurr[][COLS], char gridNext[][COLS],const int rule[2][9]);
 int neighbors(unsigned x, unsigned y, char grid[][COLS]);
